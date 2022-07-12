366f516b89829f0f73fd0698cb23733090b0a45a
”Osipov <osss151095@gmail.com>
2022-07-12 09:21:32 +0300

    добавил кнопку очистить

--- app/src/main/java/com/example/testbinding/MainActivity.kt
+++ app/src/main/java/com/example/testbinding/MainActivity.kt
@@ -1,31 +1,42 @@
 package com.example.testbinding
-
-import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import com.example.testbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
     var viewModel = TestViewModel()
-   lateinit var editText: EditText
-   lateinit var textView: TextView
+    lateinit var editText: EditText
+    lateinit var textView: TextView
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         editText = binding.editTextTextPersonName
         textView = binding.textView
+        button =   binding.button
         setContentView(binding.root)
-        viewModel.bind(owner = this, editText)
-        viewModel.liveData.observe(this,{
+
+   fun MutableLiveData<String>.bind(owner: LifecycleOwner, editText: EditText){
+       val binder = Binder(owner,this) {text -> editText.setText(text) }
+       editText.doAfterTextChanged { text -> binder.setValue(text.toString()) }
+   }
+
+        viewModel.liveData.bind(this,editText)
+        viewModel.liveData2.observe(this,{
             textView.setText(it)
         })
 
+        button.setOnClickListener {
+            viewModel.liveData.value = ""
+        }
     }
 }
--- app/src/main/java/com/example/testbinding/TestViewModel.kt
+++ app/src/main/java/com/example/testbinding/TestViewModel.kt
@@ -1,25 +1,19 @@
 package com.example.testbinding
 
 
-import android.widget.EditText
-import androidx.core.widget.doAfterTextChanged
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 
+
 class TestViewModel : ViewModel(){
-var liveData = MutableLiveData<String>()
+    var liveData = MutableLiveData<String>()
+    var liveData2 = MutableLiveData<String>()
 
-
-    fun bind(owner: LifecycleOwner, editText: EditText){
-        val binder = Binder(owner,liveData){text->
-            editText.setText(text)
-        }
-        editText.doAfterTextChanged { text ->
-            binder.setValue(text.toString().uppercase())
-        }
+ init {
+    liveData.observeForever{
+        liveData2.value = it.uppercase()
     }
+ }
 
 }
 
